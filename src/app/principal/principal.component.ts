442ae67ac5e428bd7ed36793dc6326c86d4fd2f2
Esley Leal Santana <esley.santana@tely.local>
2024-08-02 18:15:41 -0300

    adicionado validacao dos inputs, botoes, alterado a logica de validacao colocando a identificacao de cto

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -13,26 +13,27 @@ export class PrincipalComponent {
   nomeDoCliente: string = '';
   nomeDoTecnico: string = '';
 
-  ont: string = 'DIGITE O FHTT DA ONU';
-  ontPatrimonio: string = 'DIGITE O PATRIMONIO DA ONU';
-
-  onu: string = 'DIGITE O FHTT DA ONU';
-  onuRoteador: string = 'DIGITE O PATRIMONIO DO ROTEADOR';
-
-  cto: string = 'DIGITE A CTO E A PORTA'
-  ceip:string = 'DIGITE A CEIP E A PORTA'
-
+  ont: string = 'FHTT/SN DA ONU:     ';
+  ontPatrimonio: string = 'PATRIMONIO DA ONU:     ';
   aparelhoSelecionado: string = '';
-  aparelhoSelecionadoOntP: string = '';
-  ctoSelecionada: string = '';
+  wifiIntegrado: string = 'WIFI INTEGRADO';
 
+  roteador: string = 'ONU + ROTEADOR'
+  onu: string = 'FHTT DA ONU: ';
+  onuRoteador: string = 'PATRIMONIO DO ROTEADOR: ';
+  aparelhoSelecionadoOntP: string = '';
+
+  cto: string = 'CTO:'
+  ceip:string = 'CEIP:'
+  ctoSelecionada: string = '';
   tipoCtoCeipSelecionado: string = '';
   valorCtoCeip: string = '';
-
-
+  valorPortaCtoCeip: number | undefined;
+  semIdentificacao: boolean = false;
 
   mostrarInputCtoCeip: boolean = true;
-  mostrarInputAS = false;
+  mostrarInputAS: boolean = false;
+  esconderInput:boolean = false;
 
   atendimentoGerado: string = '';
 
@@ -54,35 +55,65 @@ export class PrincipalComponent {
     const valorSelecionado = event.target.value;
     if (valorSelecionado === 'cto') {
       this.tipoCtoCeipSelecionado = this.cto;
+      this.esconderInput = true;
       this.mostrarInputCtoCeip = true;
     } else if (valorSelecionado === 'ceip') {
       this.tipoCtoCeipSelecionado = this.ceip;
+      this.esconderInput = true;
       this.mostrarInputCtoCeip = true;
     } else if (valorSelecionado === 'ctoSemIdentificacao') {
+      this.esconderInput = true;
       this.mostrarInputCtoCeip = false;
     }
   }
 
-  gerarAtendimento() {
-    // Detalhes básicos do atendimento
 
-    if(this.mostrarInputCtoCeip) {
-      this.atendimentoGerado += `
+
+  gerarAtendimento() {
+
+    console.log('Função gerarAtendimento chamada');
+
+
+    // Normalizando strings para comparação
+    const aparelho = this.aparelhoSelecionado.trim().toLowerCase();
+    const ont = this.ont.trim().toLowerCase();
+    const onu = this.onu.trim().toLowerCase();
+
+    const identificacao = this.semIdentificacao
+        ? `SEM IDENTIFICAÇÃO - PORTA: ${this.valorPortaCtoCeip}`
+        : `${this.tipoCtoCeipSelecionado} - ${this.valorCtoCeip} - PORTA: ${this.valorPortaCtoCeip}`;
+
+
+    if (aparelho.includes(ont)) {
+      this.atendimentoGerado = `
       DATA 30/07/2024:
 
       ATIVAÇÃO REALIZADA COM SUCESSO!
 
       NOME DO CLIENTE: ${this.nomeDoCliente}
       NOME DO TECNICO: ${this.nomeDoTecnico}
-      EQUIPAMENTO: ${this.valorCtoCeip}
+      ${this.wifiIntegrado}
+      ${this.aparelhoSelecionado}
+      ${this.aparelhoSelecionadoOntP}
+      ${identificacao}
       `;
-    } 
+    } else if (aparelho.includes(onu)) {
+      this.atendimentoGerado = `
+      DATA 30/07/2024:
 
+      ATIVAÇÃO REALIZADA COM SUCESSO!
 
+      NOME DO CLIENTE: ${this.nomeDoCliente}
+      NOME DO TECNICO: ${this.nomeDoTecnico}
+      ${this.roteador}
+      ${this.aparelhoSelecionado}
+      ${this.aparelhoSelecionadoOntP}
+      ${identificacao}
+      `;
+    } else {
+      this.atendimentoGerado = 'Erro: Aparelho não reconhecido ou informação insuficiente.';
+    }
 
-
-    // Log para depuração
-    console.log(this.atendimentoGerado);
+    console.log('Atendimento Gerado:', this.atendimentoGerado);
   }
-
 }
